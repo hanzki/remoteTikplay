cfed976ced208b9aac7ff7bbbc8ea06809dde157
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2015-01-15 01:38:38 +0200

    Improved config and added simple ssh connectivity

--- app/main.go
+++ app/main.go
@@ -1,23 +1,83 @@
 package main
 
 import (
+	"bufio"
 	"code.google.com/p/gcfg"
 	"fmt"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
-type Config struct {
-	Basic struct {
-		Message string
+type (
+	Config struct {
+		Ssh     sshSection
+		Tikplay tikplaySection
+	}
+
+	sshSection struct {
+		Host     string
+		Port     int
+		Username string
+		Password string
+	}
+
+	tikplaySection struct {
+		Host string
+		Port int
+	}
+)
+
+var (
+	defaultConfig = Config{
+		sshSection{Host: "kekkonen.cs.hut.fi", Port: 22},
+		tikplaySection{Host: "tikradio.tt.hut.fi", Port: 5000},
+	}
+)
+
+func handleError(e error, s string, f bool) {
+	if e != nil {
+		fmt.Printf("%s: %s\n", s, e.Error())
+		if f {
+			os.Exit(1)
+		}
 	}
 }
 
 func main() {
-	var cfg Config
+	cfg := defaultConfig
 	err := gcfg.ReadFileInto(&cfg, "config.gcfg")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	handleError(err, "config", true)
+
+	sshcfg := &ssh.ClientConfig{
+		User: cfg.Ssh.Username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(cfg.Ssh.Password),
+		},
 	}
-	fmt.Println(cfg.Basic.Message)
+
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", cfg.Ssh.Host, cfg.Ssh.Port), sshcfg)
+
+	handleError(err, "ssh client", true)
+
+	tunnel, err := client.Dial("tcp", fmt.Sprintf("%s:%d", cfg.Tikplay.Host, cfg.Tikplay.Port))
+
+	handleError(err, "tunnel", true)
+
+	tunnelReader := bufio.NewReader(tunnel)
+
+	request, err := http.NewRequest("GET", "/srv/v1.0/song", nil)
+	handleError(err, "Request", true)
+
+	request.Write(tunnel)
+	response, err := http.ReadResponse(tunnelReader, request)
+	handleError(err, "Response", true)
+	if response != nil {
+		body, err := ioutil.ReadAll(response.Body)
+		response.Body.Close()
+		handleError(err, "Body", true)
+		fmt.Printf("%s\n", body)
+	}
+
 }
